53e7ad6c83514053f873e03efd435bd04fc04ea3
Brian Chen <brichen95@gmail.com>
2023-02-22 19:02:40 -0500

    final edits to build script

--- build_and_push_image.sh
+++ build_and_push_image.sh
@@ -4,24 +4,36 @@ registry_host=10.0.1.3
 registry_port=5050
 image_name=brianchen.me
 
-if ! [ -z "$2" ]; then 
-    echo "image name is $2"
-    image_name=$2
-fi
+usage() {
+    echo "Usage: $0 [-n <image name, default brianchen.me]"
+}
 
-if [ -z "$1" ]; then 
-    echo "specify a tag please!"
-    curl -X GET "$registry_host:$registry_port/v2/$image_name/tags/list"
-    exit 1
+while getopts ":n:t:" o; do
+    case "${o}" in
+        n)
+            n=${OPTARG}
+            ;;
+        t)
+            t=${OPTARG}
+            ;;
+        *)
+            usage
+            exit
+    esac
+done
+
+if ! [ -z "${n}" ]; then 
+    echo "image name is ${n}"
+    image_name=${n}
 fi
 
 full_image="$registry_host:$registry_port/$image_name"
-read -p "Build and push $full_image:$1? " -n 1 -r
+read -p "Build and push $full_image:latest? " -n 1 -r
 echo "" 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    docker image build --platform=linux/amd64 -t $full_image:$1 .
-    docker image tag -t $full_image:$1 $full_image:latest
-    docker image push $full_image:$1
+    docker image build --platform=linux/amd64 -t $full_image:$latest .
+    docker image tag -t $full_image:latest
+    docker image push $full_image:latest
 else
     echo "Only 'Y' or 'y' accepted."
 fi
